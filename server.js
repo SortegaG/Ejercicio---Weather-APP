fc06a308d8814c94e3a79da55d62e42310d3a665
Sergio Ortega <ortegagarrido.sergio@gmail.com>
2024-11-05 17:15:33 +0100

    Problema resuelto

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 const bodyParser = require('body-parser');
-const request = require('request');
-//const argv = require('yargs').argv;
+let request = require('request');
+const argv = require('yargs').argv;
 require('dotenv').config()
 
+const apiKey = process.env.API_KEY
 
-let apiKey = process.env.API_KEY;
 
-
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
 
-app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    res.render('index');
-});
-
+    res.render('index', { weather: null, error: null });
+})
 
 app.post('/', function (req, res) {
     let city = req.body.city;
     let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
     request(url, function (err, response, body) {
         if (err) {
             res.render('index', { weather: null, error: 'Error, please try again' });
@@ -37,7 +36,6 @@ app.post('/', function (req, res) {
     });
 })
 
-
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
-});
+})
